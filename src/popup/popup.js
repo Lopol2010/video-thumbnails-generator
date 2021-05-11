27987ad056eb5b263f2d280f8a4242ea6c0131f3
Evgeniy <evgeniy7@gmail.com>
2021-05-11 04:29:07 +0300

    added chrome api typesA

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,2 +1,26 @@
 import "./popup.sass"
 let m = require('mithril')
+
+
+
+function getTabId()
+{
+    
+}
+
+
+let App = {
+    view: function () {
+        return (
+            <div>
+                <p>
+                    Enabled on this site?
+                    <input type="checkbox" id="enabled" onclick ={ function () { console.log(getTabId()) } }/>
+                </p>
+            </div>
+        )
+    }
+}
+
+
+m.render(document.body, <App/>)
